4b4c589a1b3b7ce877d5479808de2f826dbddd99
Pierre Lourens <plourens3@gmail.com>
2023-04-07 08:42:32 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+
+  return (
+    <h1 className="text-4xl font-bold underline">
+      Hello world! There
+    </h1>
+  )
+}
+
+export default App
