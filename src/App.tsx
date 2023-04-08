f4b013cff04f7bbb7d5cdcc6b8f4c1dcfc217d0c
Pierre Lourens <plourens3@gmail.com>
2023-04-08 11:57:21 -0700

    use tailwind to set bg color

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './index.css'
-import Card from './components/Card'
+import './index.css';
+import Card from './components/Card';
 
 function App() {
-
   return (
-    <Card />
-  )
+    <div className="h-screen bg-light-gray">
+      <Card />
+    </div>
+  );
 }
 
-export default App
+export default App;
